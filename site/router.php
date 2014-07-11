70d47475ddec57138536b76647d9e4b810a46694
Alonzo Turner <alonzo.turner@subtextproductions.com>
2014-07-11 15:40:19 -0500

    FIXED: Update router to modern standards

--- site/router.php
+++ site/router.php
@@ -11,36 +11,26 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 function MapsBuildRoute(&$query){
-	$mainframe = JFactory::getApplication();
-	$menu = $mainframe->getMenu();
-	$link = "index.php?option=com_maps&id={$query['id']}";
 	$segments	= array();
-	$segments[] = $query['id'];
-	unset($query['id']);
-	//$mainframe->close();
+	if(!empty($segments['layout'])){
+		$segments[] = $query['layout'];
+		unset($query['layout']);
+	}
+	if(!empty($segments['id'])){
+		$segments[] = $query['id'];
+		unset($query['id']);
+	}
 	if(empty($query['Itemid'])){
 		$params = JComponentHelper::getParams('com_maps');
-		$query['Itemid'] = $params->get('default_id');
-	}else{
-		$active = $menu->getActive();
-		if($active->id == $query['Itemid']){
-			$item = $menu->getItems('link', $link, true);
-			if(!is_null($item)){
-				$query['Itemid'] = $item->id;
-				$segments = array();
-			}else{
-				$params = JComponentHelper::getParams('com_maps');
-				$query['Itemid'] = $params->get('default_id');
-			}
-		}
+		$query['Itemid'] = $params->get('params.default_id');
 	}
 	return $segments;
 }
 
 function MapsParseRoute($segments){
 	$query	= array();
-	$query['view'] = 'maps';
-	$query['id'] = array_shift(explode(":", $segments[0]));
+	$query['layout'] = $segments[0];
+	$query['id'] = array_shift(explode(":", $segments[1]));
 
 	return $query;
 }

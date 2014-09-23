8ef7e58812fe5b79a0e6e253b276822131df3423
Alonzo Turner <alonzo.turner@subtextproductions.com>
2014-09-23 13:07:19 -0500

    CHANGED: Use HTML5 data attributes
    This used to use regular expressions to load data about the map. This
    format will now be compatible with both MooTools and jQuery.

--- site/views/maps/tmpl/default.php
+++ site/views/maps/tmpl/default.php
@@ -13,23 +13,22 @@
 	if(trim($this->map->meta_description)){
 		$document->setMetaData('description', $this->map->meta_description);
 	}
-	$style = "";
+	$style = "#map-id-".$this->map->map_id." {";
 	if($width = $this->params->get('map_width', 0)){
 		$style .= "width: {$width}px;";
 	}
 	if($height = $this->params->get('map_height', 0)){
 		$style .= "height: {$height}px;";
 	}
-	$uri		= JURI::getInstance();
-	$base		= $uri->root();
+	$style .= "}";
+	$document->addStyleDeclaration($style);
 	if($this->params->get('show_title')){
 ?>
 
 <h1><? echo $this->map->map_name; ?></h1>
 
 <?	} ?>
-
-<div class="google-map_ id<? echo $this->map->map_id; ?> zoom<? echo $this->params->get('zoom'); ?> lat<? echo $this->params->get('center_lat'); ?> lng<? echo $this->params->get('center_lng'); ?>" style="<? echo $style; ?>"></div>
+<div class="mapbuilder" id="map-id-<? echo $this->map->map_id; ?>" data-id="<? echo $this->map->map_id; ?>" data-zoom="<? echo $this->params->get('zoom'); ?>" data-lat="<? echo $this->params->get('center_lat'); ?>" data-lng="<? echo $this->params->get('center_lng'); ?>"></div>
 
 <?	if($this->params->get('show_description')){ ?>
 <p><? echo $this->map->map_description; ?></p>

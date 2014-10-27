dfa8c83e549b3b259a01cc9678f7129e87cd948c
Alonzo Turner <alonzo.turner@subtextproductions.com>
2014-10-27 01:45:29 -0500

    FIXED: Update to use the legacy mvc structure

--- site/mapbuilder.php
+++ site/mapbuilder.php
@@ -13,27 +13,14 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 // DEFINE DS CONSTANT
 if(!defined('DS')) define( 'DS', DIRECTORY_SEPARATOR );
 
-// REQUIRE THE BASE CONTROLLER
-require_once( JPATH_COMPONENT.DS.'controller.php' );
- 
-// REQUIRE SPECIFIC CONTROLLER IF REQUESTED
-if($controller = JRequest::getVar('controller')) {
-    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
-    if (file_exists($path)) {
-        require_once $path;
-    } else {
-        $controller = '';
-    }
-}
- 
-// CREATE THE CONTROLLER
-$classname    = 'MapBuilderController'.$controller;
-$controller   = new $classname();
+// IMPORT CONTROLLER LIBRARY
+jimport('joomla.application.component.controller');
+
+// GET CONTROLLER INSTANCE
+$controller = JControllerLegacy::getInstance('MapBuilder');
  
 // PERFORM THE REQUEST TASK
-if($controller->execute(JRequest::getVar('task')) === false){
-	$controller->setRedirect(JRoute::_("index.php?option=com_mapbuilder"), $controller->getError(), "error");
-}
- 
+$controller->execute(JRequest::getVar('task'));
+
 // REDIRECT IF SET BY THE CONTROLLER
 $controller->redirect();
